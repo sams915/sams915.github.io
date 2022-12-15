ace3eda725ff3e798bad8775dd2f023af4f46695
Sam Hefford <sams915@hotmail.co.uk>
2022-12-15 17:04:48 +0000

    fade in 1

---
+++ scripts/opacity-observer.js
@@ -0,0 +1,27 @@
+/*eslint-env es6*/
+/*eslint-env browser*/
+
+const faders = document.querySelectorAll('.fade-in');
+
+const fadersOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -400px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+    entries,
+    appearOnScroll
+) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+}, fadersOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
